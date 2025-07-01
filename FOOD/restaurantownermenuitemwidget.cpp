9c47b449127d9146ebe0bb91baff741a9e28dcd9
Parnian Taheri <prniantahry@gmail.com>
2025-07-01 14:22:02 +0330

    some ui edits

--- FOOD/restaurantownermenuitemwidget.cpp
+++ FOOD/restaurantownermenuitemwidget.cpp
@@ -1,3 +1,28 @@
 #include "restaurantownermenuitemwidget.h"
+#include "ui_restaurantownermenuitemwidget.h"
 
-restaurantownermenuitemwidget::restaurantownermenuitemwidget() {}
+RestaurantOwnerMenuItemWidget::RestaurantOwnerMenuItemWidget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::restaurantownermenuitemwidget)
+{
+    ui->setupUi(this);
+
+    connect(ui->pushButton, &QPushButton::clicked, this, &RestaurantOwnerMenuItemWidget::on_removeButton_clicked);
+}
+
+RestaurantOwnerMenuItemWidget::~RestaurantOwnerMenuItemWidget()
+{
+    delete ui;
+}
+
+void RestaurantOwnerMenuItemWidget::setItemInfo(const QString &name, const QString &description, const QString &priceText)
+{
+    ui->name->setText(name);
+    ui->description->setText(description);
+    ui->price->setText(priceText);
+}
+
+void RestaurantOwnerMenuItemWidget::on_removeButton_clicked()
+{
+    emit removeRequested(); // وقتی کاربر روی دکمه حذف میزنه
+}
--- FOOD/restaurantownermenuitemwidget.h
+++ FOOD/restaurantownermenuitemwidget.h
@@ -1,10 +1,30 @@
 #ifndef RESTAURANTOWNERMENUITEMWIDGET_H
 #define RESTAURANTOWNERMENUITEMWIDGET_H
 
-class restaurantownermenuitemwidget
+#include <QWidget>
+
+namespace Ui {
+class restaurantownermenuitemwidget;
+}
+
+class RestaurantOwnerMenuItemWidget : public QWidget
 {
+    Q_OBJECT
+
 public:
-    restaurantownermenuitemwidget();
+    explicit RestaurantOwnerMenuItemWidget(QWidget *parent = nullptr);
+    ~RestaurantOwnerMenuItemWidget();
+
+    void setItemInfo(const QString &name, const QString &description, const QString &priceText);
+
+signals:
+    void removeRequested();  // سیگنال برای وقتی که دکمه حذف زده میشه
+
+private slots:
+    void on_removeButton_clicked(); // اسلات برای دکمه حذف
+
+private:
+    Ui::restaurantownermenuitemwidget *ui;
 };
 
 #endif // RESTAURANTOWNERMENUITEMWIDGET_H
