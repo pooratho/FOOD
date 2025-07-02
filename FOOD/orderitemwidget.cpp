44741065b72ba465afb5bc49fa40985fbcc8d4a9
Parnian Taheri <prniantahry@gmail.com>
2025-07-02 19:28:59 +0330

    disign UML for base classes

--- FOOD/orderitemwidget.cpp
+++ FOOD/orderitemwidget.cpp
@@ -1,5 +1,23 @@
+// orderitemwidget.cpp
 #include "orderitemwidget.h"
 
-orderitemwidget::orderitemwidget(QWidget *parent)
-    : QWidget{parent}
-{}
+OrderItemWidget::OrderItemWidget(QWidget *parent) :
+    QWidget(parent)
+{
+    ui.setupUi(this);
+}
+
+OrderItemWidget::~OrderItemWidget()
+{
+}
+
+// متدهای تنظیم نام و کد
+void OrderItemWidget::setName(const QString& name)
+{
+    ui.name->setText(name);
+}
+
+void OrderItemWidget::setCode(const QString& code)
+{
+    ui.code->setText(code);
+}
--- FOOD/orderitemwidget.h
+++ FOOD/orderitemwidget.h
@@ -1,15 +1,24 @@
+// orderitemwidget.h
 #ifndef ORDERITEMWIDGET_H
 #define ORDERITEMWIDGET_H
 
 #include <QWidget>
+#include "ui_orderitemwidget.h"
 
-class orderitemwidget : public QWidget
+class OrderItemWidget : public QWidget
 {
     Q_OBJECT
-public:
-    explicit orderitemwidget(QWidget *parent = nullptr);
 
-signals:
+public:
+    explicit OrderItemWidget(QWidget *parent = nullptr);
+    ~OrderItemWidget();
+
+    // می‌توانید متدهایی برای تنظیم داده‌ها اضافه کنید
+    void setName(const QString& name);
+    void setCode(const QString& code);
+
+private:
+    Ui::orderitemwidget ui;
 };
 
 #endif // ORDERITEMWIDGET_H
